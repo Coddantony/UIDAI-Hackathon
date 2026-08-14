266bdce184bf56d1110589e78a7862e20f1c8284
Coddantony <coddantony@gmail.com>
2026-08-15 00:20:01 +0530

    Add Aadhaar Verhoeff validation

---
+++ Web/aadhar-authenticator/utils/aadhaar.py
@@ -0,0 +1,30 @@
+_D = (
+    (0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
+    (1, 2, 3, 4, 0, 6, 7, 8, 9, 5),
+    (2, 3, 4, 0, 1, 7, 8, 9, 5, 6),
+    (3, 4, 0, 1, 2, 8, 9, 5, 6, 7),
+    (4, 0, 1, 2, 3, 9, 5, 6, 7, 8),
+    (5, 9, 8, 7, 6, 0, 4, 3, 2, 1),
+    (6, 5, 9, 8, 7, 1, 0, 4, 3, 2),
+    (7, 6, 5, 9, 8, 2, 1, 0, 4, 3),
+    (8, 7, 6, 5, 9, 3, 2, 1, 0, 4),
+    (9, 8, 7, 6, 5, 4, 3, 2, 1, 0),
+)
+_P = (
+    (0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
+    (0, 5, 7, 6, 2, 1, 4, 3, 9, 8),
+)
+_INV = (0, 4, 3, 2, 1, 5, 6, 7, 8, 9)
+
+
+def is_valid_aadhaar(value: str) -> bool:
+    """Validate 12-digit Aadhaar format and Verhoeff checksum."""
+    digits = value.replace(" ", "")
+    if len(digits) != 12 or not digits.isdigit():
+        return False
+    if digits[0] == "0":
+        return False
+    checksum = 0
+    for index, digit in enumerate(reversed(digits)):
+        checksum = _D[checksum][_P[index % 8][int(digit)]]
+    return checksum == 0
