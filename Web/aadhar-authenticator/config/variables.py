c43e812f93d09027816cf3c5bda0e467279e8631
Coddantony <coddantony@gmail.com>
2026-08-15 00:10:07 +0530

    Make JWT settings production-safe

--- Web/aadhar-authenticator/config/variables.py
+++ Web/aadhar-authenticator/config/variables.py
@@ -10,10 +10,13 @@ MONGO_CONFIG = {
 
 JWT_CONFIG = {
     "SECRET_KEY": os.getenv("SECRET_KEY"),
-    "ALGORITHM": "HS256",
-    "ACCESS_TOKEN_EXPIRE_MINUTES": 60,
-    "REFRESH_TOKEN_EXPIRE_MINUTES": 360,
-    "GUEST_TOKEN_EXPIRE_MINUTES": 360
+    "ALGORITHM": os.getenv("JWT_ALGORITHM", "HS256"),
+    "ACCESS_TOKEN_EXPIRE_MINUTES": int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30")),
+    "REFRESH_TOKEN_EXPIRE_MINUTES": int(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES", "360")),
+    "GUEST_TOKEN_EXPIRE_MINUTES": int(os.getenv("GUEST_TOKEN_EXPIRE_MINUTES", "60")),
 }
+
+if not JWT_CONFIG["SECRET_KEY"] and os.getenv("ENVIRONMENT", "development").lower() == "production":
+    raise RuntimeError("SECRET_KEY must be configured in production")
 
 SAMPLE_XML = '<OfflinePaperlessKyc referenceId="00000"></OfflinePaperlessKyc>'
