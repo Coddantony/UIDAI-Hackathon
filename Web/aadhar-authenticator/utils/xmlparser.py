e84b7af841183cb93f6d7fda43ec217935f1070a
Coddantony <coddantony@gmail.com>
2026-08-14 23:56:34 +0530

    Harden eKYC XML parsing

--- Web/aadhar-authenticator/utils/xmlparser.py
+++ Web/aadhar-authenticator/utils/xmlparser.py
@@ -1,17 +1,23 @@
 import xml.etree.ElementTree as ET
 
 
-def parse_xml(data):
-    """
-    Parse xml data
-    """
-    user_data = {}
-    fields = ["name", "dob", "gender"]
-    try:
-        tree = ET.fromstring(data)
-        for field in fields:
-            user_data[field] = tree.find('UidData/Poi').attrib[field]
-    except Exception as e:
-        print(e)
-    return user_data
+ALLOWED_FIELDS = ("name", "dob", "gender")
 
+
+def parse_xml(data):
+    """Extract the supported identity attributes from an eKYC XML payload."""
+    if not data or not isinstance(data, str):
+        return {}
+
+    try:
+        root = ET.fromstring(data)
+        poi = root.find("UidData/Poi")
+        if poi is None:
+            return {}
+        return {
+            field: poi.attrib[field]
+            for field in ALLOWED_FIELDS
+            if field in poi.attrib
+        }
+    except (ET.ParseError, ValueError, TypeError):
+        return {}
